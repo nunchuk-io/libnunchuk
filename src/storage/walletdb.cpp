4a4de266f375fdab48040a5bdf599c3bab3d1628
Bakaoh <batatsar@gmail.com>
2023-01-10 21:16:51 +0700

    Fix forceRefresh bug

--- src/storage/walletdb.cpp
+++ src/storage/walletdb.cpp
@@ -1072,8 +1072,8 @@ void NunchukWalletDb::FillSendReceiveData(Transaction& tx) {
 }
 
 void NunchukWalletDb::ForceRefresh() {
-  SQLCHECK(sqlite3_exec(db_, "DELETE * FROM VTX;", NULL, 0, NULL));
-  SQLCHECK(sqlite3_exec(db_, "DELETE * FROM ADDRESS;", NULL, 0, NULL));
+  SQLCHECK(sqlite3_exec(db_, "DELETE FROM VTX;", NULL, 0, NULL));
+  SQLCHECK(sqlite3_exec(db_, "DELETE FROM ADDRESS;", NULL, 0, NULL));
   addr_cache_.erase(db_file_name_);
 }
 
